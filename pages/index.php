f359cdf68abb206c0522a022d71e6e8b49af0be0
Hongbin Lee <ksmlee0531@gmail.com>
2020-09-06 13:50:56 +0900

    add 1 paragraph about me and links and fix some error

--- pages/index.php
+++ pages/index.php
@@ -10,9 +10,10 @@
             </header>
             <nav>
                 <?php include("../php/nav.php");
-                visiting("MAIN");?>
+                visiting("MAIN"); # make visiting link unclickable ?>
             </nav>
             <main>
+                <p id="mainpic"><img src="../pngfiles/mainpic.jpg" alt="avatar"></p>
                 <div id="pInfo-Box">
                     <div class="pPics"><img src="../pngfiles/redbin.png" alt="Redbin-pic" width="100" height="100"></div> <!-- 여기는 my 사진 -->
                     <div id="pInfo">
@@ -22,6 +23,13 @@
                         <div class="pInfo-category">Marital Status:</div> <div class="pInfo-ans">Married</div>
                         <div class="pInfo-category">Native:</div> <div class="pInfo-ans">Korean</div>                    
                     </div>
+                </div>
+                <div id="object">
+                    <blockquote>
+                        I am looking for a entry position of software engineer focusing on software development or data analyst.<br>
+                        I have been studying about computer science, mathematics, and information system.<br>
+                        I want to find a place to put my effort to help others and strengthen the team I work with.
+                    </blockquote>
                 </div>
                 <div id="lifeEvent-Box">
                     <div class="left container">
@@ -105,5 +113,22 @@
                         </div>
                     </div>
                 </div>
+                <div id="links" class="linkIvisit">
+                    <ul>
+                        <li> Links of Korea, Republic of
+                            <ul>
+                                <li><a href="#">Programmers</a></li>
+                                <li><a href="#">Saramin</a></li>
+                            </ul>
+                        </li>
+                        <li> Links of United States
+                            <ul>
+                                <li><a href="#">HackerRank</a></li>
+                                <li><a href="#">LinkedIn</a></li>
+                                <li><a href="#">Handshake</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
             </main>
-        <?php include("../php/footer.php");?>
+        <?php include("../php/footer.php"); // import footer php?> 
--- php/footer.php
+++ php/footer.php
@@ -1,5 +1,5 @@
 <footer>
-                <div id="copyright"><sapan>&copy; Redbin Lee</sapan></div>
+                <div id="copyright"><span>&copy; Redbin Lee</span></div>
                 <div>
                     <span class = "footer_logo"><a href="https://www.facebook.com/profile.php?id=100008562494600"><i class="fab fa-facebook-square fa-2x"></i></a><!-- sns logo 삽입 -->
                     <a href="www.linkedin.com/in/hongbin-lee"><i class="fab fa-linkedin fa-2x"></i></a></span>
