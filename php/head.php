e39a7225e72ca8248e7467c5e8d1db1c76f7b426
Hongbin Lee <ksmlee0531@gmail.com>
2020-09-16 13:03:03 +0900

    add db codes on head.php

--- php/head.php
+++ php/head.php
@@ -8,3 +8,18 @@
 <link rel="stylesheet" href="../css/index.css">
 <link rel="shortcut icon" href="#">
 <!--dbpassword: CeI}q*xUE+#N -->
+<?php
+  function connectDB(){
+    $db = mysqli_connect("localhost", "leehb_portfolio_user", "CeI}q*xUE+#N", "leehb_portfolio");
+    return $db;
+  }
+  $db = connectDB();
+  $sql = "INSERT INTO users (name,email,comment) VALUES ('".$_POST['name']."',";
+  $sql .= "'".$_POST['email']."',";
+  $sql .= "'".$_POST['message']."');";
+  print $sql;
+  $result = mysqli_query($db, $sql);
+  if(!$result){
+    print("Error description: " . mysqli_error($db));
+  }
+?>
