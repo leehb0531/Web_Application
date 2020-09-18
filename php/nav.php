f8c6493d7eb2ac35df6e9addd653eb41409d9194
Hongbin Lee <ksmlee0531@gmail.com>
2020-09-18 14:18:06 +0900

    add answering the questions page for database assignment

--- php/nav.php
+++ php/nav.php
@@ -8,24 +8,28 @@ function visiting($page) { ?>
     <div class="subnav"><a href="form.php">SIGNUP&CONTACT</a>
       <div class="sub_content"><a href="../pages/userlist.php">Userlist</a></div>
     </div>
+    <div class="subnav"><a href="../pages/assignment.php">Answer for Assginment</a></div>
   <?php } elseif ($page =="WORDS"){ ?>
     <div class="subnav"><a href="index.php">MAIN</a></div>
     <div id="visiting" class="subnav"><a href="wordsMain.php">WORDS</a></div>
     <div class="subnav"><a href="form.php">SIGNUP&CONTACT</a>
       <div class="sub_content"><a href="../pages/userlist.php">Userlist</a></div>
     </div>
+    <div class="subnav"><a href="../pages/assignment.php">Answer for Assginment</a></div>
   <?php } elseif ($page=="otherDir"){?>
     <div class="subnav"><a href="../pages/index.php">MAIN</a></div>
     <div class="subnav"><a href="../pages/wordsMain.php">WORDS</a></div>
     <div class="subnav"><a href="../pages/form.php">SIGNUP&CONTACT</a>
       <div class="sub_content"><a href="../pages/userlist.php">Userlist</a></div>
     </div>
+    <div class="subnav"><a href="../pages/assignment.php">Answer for Assginment</a></div>
   <?php }  else{?>
     <div class="subnav"><a href="../pages/index.php">MAIN</a></div>
     <div class="subnav"><a href="../pages/wordsMain.php">WORDS</a></div>
     <div class="subnav"><a href="../pages/form.php">SIGNUP&CONTACT</a>
       <div class="sub_content"><a href="../pages/userlist.php">Userlist</a></div>
     </div>
+    <div class="subnav"><a href="../pages/assignment.php">Answer for Assginment</a></div>
   <?php } ?>
   </div>
 <?php } ?>
